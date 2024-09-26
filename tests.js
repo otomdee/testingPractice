78c13cff773fc1d0213c12a0d8cf88051fdbc07a
otomdee <otomdaniel65@gmail.com>
2024-09-26 10:51:37 +0100

    add cipher tests

--- tests.js
+++ tests.js
@@ -9,3 +9,41 @@ export function reverseString(string) {
     }
     return revString;
 }
+
+export const calculator = {
+    add : (a,b) => {
+        return a + b;
+    },
+    subtract: (a,b) => {
+        return a - b;
+    },
+    divide: (a,b) => {
+        return a / b;
+    },
+    multiply: (a,b) => {
+        return a * b;
+    }
+}
+
+export function caesarCipher(string, key) {
+    let cipher = "";
+    for (let i = 0; i < string.length; i++) {
+        cipher = cipher.concat(shiftChar(string[i], key));
+    }
+    return cipher;
+}
+
+function shiftChar(char, key) {
+    let charCode = char.charCodeAt(0);
+
+    // Check if the character is lowercase
+  if (char >= 'a' && char <= 'z') {
+    return String.fromCharCode(((charCode - 97 + key) % 26) + 97);
+  }
+  else if (char >= 'A' && char <= 'Z') {
+    return String.fromCharCode(((charCode - 65 + key) % 26) + 65);
+  } else {
+    // If not an alphabet, return the character as is
+    return char;
+  }
+}
--- tests.test.js
+++ tests.test.js
@@ -1,6 +1,8 @@
 import {
     capitalize,
-    reverseString
+    reverseString,
+    calculator,
+    caesarCipher
 } from "./tests.js";
 
 test('capitalize', () => {
@@ -9,4 +11,36 @@ test('capitalize', () => {
 
 test('reverse', () => {
     expect(reverseString("goat")).toBe("taog");
+})
+
+test("add", () => {
+    expect(calculator.add(1,2)).toBe(3);
+})
+
+test("subtract", () => {
+    expect(calculator.subtract(3,1)).toBe(2);
+})
+
+test("divide", () => {
+    expect(calculator.divide(6,3)).toBe(2);
+})
+
+test("multiply", () => {
+    expect(calculator.multiply(100,2)).toBe(200);
+})
+
+test("cipher", () => {
+    expect(caesarCipher("cooking", 3)).toBe("frrnlqj");
+})
+
+test("cipher wrapping", () => {
+    expect(caesarCipher("xyz", 3)).toBe("abc");
+})
+
+test("cipher case preservation", () => {
+    expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+})
+
+test("cipher case preservation", () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 })
